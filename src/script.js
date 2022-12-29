c8e1db07fe621a5da0e4e15711ff0a271c4b960c
KB <kj.br@protonmail.com>
2022-12-29 17:22:53 -0600

    Update Script.js

--- src/script.js
+++ src/script.js
@@ -1,14 +1,3 @@
-/* eslint-disable max-classes-per-file */
-import { gameBoardConfig } from './app/game-components/config/gameBoard.config.js';
-import GameBoard from './app/game-components/gameboard.class.js';
-import { GameLoop, newGame } from './app/game-functions/gameloop.class.js';
-
-// const gameBoard = new GameBoard(gameBoardConfig);
-// gameBoard.build();
-// console.dir(gameBoard);
-
-// const gameLoop = new GameLoop(gameBoard);
-// gameLoop.gameInit();
+import { newGame } from './app/game-functions/gameloop.class.js';
 
 newGame();
-// put the whole thing in a global function for restart
