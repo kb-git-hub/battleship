707c967f49a199d8f1a0f7e26b96bb7a720e391a
KB <kj.br@protonmail.com>
2022-12-28 20:45:32 -0600

    Update GameLoop

--- src/app/game-functions/gameloop.js
+++ src/app/game-functions/gameloop.js
@@ -3,13 +3,11 @@ function gameInit(gameBoard) {
 }
 
 function addPlayerRotateShipListener(gameBoard) {
-    console.log(gameBoard);
     window.addEventListener('keypress', (e) => {
         const {
             players: { player },
         } = gameBoard;
         if (e.key === 'r') {
-            console.log('activate rotate');
             if (player.placingShip === 'horizontal') player.placingShip = 'vertical';
             else if (player.placingShip === 'vertical') player.placingShip = 'horizontal';
         }
